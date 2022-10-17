ab761988791bbe5b952decb73071a9b674120356
gabrgoli <ggoliger@yahoo.com>
2022-10-17 11:52:18 -0300

    fotter design

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -249,15 +249,18 @@ const Home = () => {
                     <div className="c-container">
                         <div className="c-footer__wrap">
                             <h1 className="footerTitle">Contact us for more information.</h1>
-                            <p className="address">
-                                <b>IAMX AG</b><br />
-                                Dammstrasse 16<br />
-                                CH-6300 Zug<br />
-                                Switzerland
-                            </p>
+
                         </div>
-                        <div className="c-footer__wrap">
-                            <a href="https://iamx.id/contact/" target="_blank"><button className="contactButton">Drop us a line</button></a>
+                        <div className="c-footer__wrap2">
+                            <div className='adressDropButton'>
+                                <a href="https://iamx.id/contact/" target="_blank"><button className="contactButton">Drop us a line</button></a>
+                                <p className="address">
+                                    <b>IAMX AG</b><br />
+                                    Dammstrasse 16<br />
+                                    CH-6300 Zug<br />
+                                    Switzerland
+                                </p>
+                            </div>
                             <div className="footer-social">
                                 <div className="holder">
                                     <a href="https://ch.linkedin.com/company/iamx-ag">
